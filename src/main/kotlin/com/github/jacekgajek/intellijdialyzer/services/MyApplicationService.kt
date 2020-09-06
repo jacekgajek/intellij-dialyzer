b81142375994569438bbbd68fa128c1367a40736
GitHub Action <action@github.com>
2020-09-06 13:37:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jacekgajek/intellijdialyzer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jacekgajek.intellijdialyzer.services
+
+import com.github.jacekgajek.intellijdialyzer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
